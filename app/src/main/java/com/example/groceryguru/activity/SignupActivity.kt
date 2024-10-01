273e3dbc5643ab7aecd899a29b2d9b35bcaa4196
SAThenuwara <sandulthenuwara@gmail.com>
2024-10-01 13:46:32 +0530

    multiple pages

--- app/src/main/java/com/example/groceryguru/activity/SignupActivity.kt
+++ app/src/main/java/com/example/groceryguru/activity/SignupActivity.kt
@@ -1,6 +1,8 @@
 package com.example.groceryguru.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,12 +12,18 @@ import com.example.groceryguru.R
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_signup)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Find the "Create Account" button by its ID
+        val createAccountButton: Button = findViewById(R.id.signUpButton)
+
+        // Set an OnClickListener on the button
+        createAccountButton.setOnClickListener {
+            // Create an Intent to start the SignupActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/groceryguru/activity/WelcomeActivity.kt
+++ app/src/main/java/com/example/groceryguru/activity/WelcomeActivity.kt
@@ -1,10 +1,9 @@
 package com.example.groceryguru.activity
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.groceryguru.R
 
 class WelcomeActivity : AppCompatActivity() {
@@ -13,6 +12,16 @@ class WelcomeActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_welcome)
 
+        // Find the "Create Account" button by its ID
+        val createAccountButton: Button = findViewById(R.id.createAccountButton)
 
+        // Set an OnClickListener on the button
+        createAccountButton.setOnClickListener {
+            // Create an Intent to start the SignupActivity
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+
+
+        }
     }
 }
